598a2e3efc263f5fe7331abafa39c9994293a26f
lblacha <lukasz.blacha@gmail.com>
2024-07-12 00:10:27 +0200

    feat: add light

--- src/sketches/shader.frag.glsl
+++ src/sketches/shader.frag.glsl
@@ -9,20 +9,33 @@ varying vec2 vPos;
 #define MAX_ITERATIONS 80
 #define MAX_DISTANCE 100.0
 #define SURFACE_DISTANCE 0.01
+#define PI 3.1415926538
 
-float opUnion(float a, float b) {
-  return min(a, b);
+float min3(float a, float b, float c) {
+  return min(a, min(b, c));
+}
+
+float min4(float a, float b, float c, float d) {
+  return min3(a, b, min(c, d));
+}
+
+float max3(float a, float b, float c) {
+  return max(a, max(b, c));
+}
+
+float max4(float a, float b, float c, float d) {
+  return max3(a, b, max(c, d));
 }
 
 float opSubtraction(float a, float b) {
-  return max(-a, b);
+  return max(a, -b);
 }
 
 float opIntersection(float a, float b) {
   return max(a, b);
 }
 
-float opSmoothUnion(float a, float b, float k) {
+float smin(float a, float b, float k) {
   float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
   return mix(b, a, h) - k * h * (1.0 - h);
 }
@@ -41,10 +54,36 @@ vec3 scaleBox (vec3 size, vec3 coeff) {
   return size * coeff;
 }
 
-mat2 rot2d(float angle) {
+vec3 mirror_x(in vec3 position) {
+    return vec3(abs(position.x), position.yz);
+}
+vec3 mirror_y(in vec3 position) {
+    return vec3(position.x, abs(position.y), position.z);
+}
+vec3 mirror_z(in vec3 position) {
+    return vec3(position.xy, abs(position.z));
+}
+
+mat2 rot2d(in float angle) {
   float s = sin(angle);
   float c = cos(angle);
   return mat2(c, -s, s, c);
+}
+
+vec3 mov_x(vec3 point, in float dist) {
+  return vec3(point.x + dist, point.yz);
+}
+
+vec3 mov_y(vec3 point, in float dist) {
+  return vec3(point.x, point.y + dist, point.z);
+}
+
+vec3 mov_z(vec3 point, in float dist) {
+  return vec3(point.xy, point.z + dist);
+}
+
+vec3 mov(vec3 point, vec3 dist) {
+  return point + dist;
 }
 
 vec3 rot_xy(vec3 p, float angle) {
@@ -65,8 +104,8 @@ vec3 rot_yz(vec3 p, float angle) {
   return p2;
 }
 
-vec3 rot_3d(vec3 p, vec3 axis, float angle) {
-  // Rodrigues' rotation formula
+// Rodrigues' rotation formula
+vec3 rot(vec3 p, vec3 axis, float angle) {
   return cross (axis, p) * sin(angle) +
     mix(
       dot(axis, p) * axis,
@@ -86,66 +125,101 @@ float sd_box(vec3 position, vec3 size) {
   return length(max(q, 0.0)) + min(max(q.x,max(q.y, q.z)), 0.0);
 }
 
-vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
-   return a + b * cos(6.28318 * (c * t + d));
-}
+float sdf_cabin(vec3 p) {
+  vec3 point = mirror_x(p);
 
-float sdf_distance(vec3 point) {
-  // sphere
-  vec3 sphere_position = vec3(3. * sin(uTime), 0, 0);
-  float sphere_radius = 0.4;
-  float sphere = sd_sphere(point - sphere_position, sphere_radius);
-
-  // box
-  vec3 box_position = vec3(0, 0, 0);
-  float box_size = 0.02;
-  vec3 point2 = point;
-  point2.y += uTime;
-  point2 = fract(point2) - 0.5;
-  point2 = rot_xz(point2, uTime * 3.0);
-  float box = sd_box(point2 - box_position, vec3(box_size));
-
-  // floor
-  float ground_plane = point.y + 5.;
-
-  return min(
-    ground_plane,
-    opSmoothUnion(
-      box,
-      sphere,
-      1.
+  return opSubtraction(
+    // corpus
+    sd_box(
+      point - vec3(0, 0, 0), // position
+      vec3(0.2, 0.2, 0.6) // size
+    ),
+    sd_box(
+      rot_xy(rot_xz(point - vec3(0.4, 0, -0.2), PI/12.), PI/12.), // position
+      vec3(0.2, 0.3, 0.6) // size
     )
   );
 }
 
-void main() {
-  vec2 uv = vPos * 2.0 - 1.0;
+float distane_to_scene(vec3 point) {
+  float cabin = sdf_cabin(point);
+  float ground = point.y + 3.1;
 
-    // Initialization
-    vec3 ray_origin = vec3(0, 0, -3);
-    // Vector length is normalized to be of length 1
-    vec3 ray_direction = normalize(vec3(uv, 1));
+  return min(
+    ground,
+    cabin
+  );
+}
 
-    // Move the camera
-    ray_origin.xz *= rot2d(-uMouse.x * 2.0);
-    ray_direction.xz *= rot2d(-uMouse.x * 2.0);
-    ray_origin.yz *= rot2d(-uMouse.y * 2.0);
-    ray_direction.yz *= rot2d(-uMouse.y * 2.0);
+vec3 normal_vector(vec3 point)
+{ 
+    float distance = distane_to_scene(point);
+    vec2 e = vec2(SURFACE_DISTANCE, 0); // Epsilon
+    vec3 n = distance - vec3(
+      distane_to_scene(point - e.xyy),  
+      distane_to_scene(point - e.yxy),
+      distane_to_scene(point - e.yyx)
+    );
+   
+    return normalize(n);
+}
 
+// @see https://www.shadertoy.com/view/NlfGDs
+float light_diffuse(vec4 light, vec3 point) { 
+    vec3 light_direction = normalize(light.xyz - point);
+   // Diffuse light
+    float dif = dot(normal_vector(point), light_direction);
+    // Clamp so it doesnt go below 0
+    return clamp(dif * light.w, 0.0, 1.0);
+}
+
+float get_light(vec3 position) {
+  vec4 light1 = vec4(rot_xz(vec3(9, 2, 0), uTime / 2.), 0.2);
+  vec4 light2 = vec4(rot_xz(vec3(-9, -2, 0), uTime / 2.), 0.2);
+  vec4 light3 = vec4(-3, 10, 0, 0.8);
+
+  float val = light_diffuse(light1, position) + light_diffuse(light2, position) + light_diffuse(light3, position);
+
+  return clamp(val, 0.0, 1.0);
+}
+
+vec4 ray_march(vec3 origin, vec3 direction) {
     // total distance travelled
     float travel_distance = 0.0;
+    vec3 position = origin;
     // Raymarching
     for (int i = 0; i < MAX_ITERATIONS; i++) {
         // position along the ray
-        vec3 ray_position = ray_origin + ray_direction * travel_distance;
-        // current distance to the scene
-        float scene_distance = sdf_distance(ray_position);
+        position = origin + direction * travel_distance;
+        float scene_distance = distane_to_scene(position);
         // advance the ray
         travel_distance += scene_distance;
         // early stop if close to the scene or too far out
         if (scene_distance < SURFACE_DISTANCE || travel_distance > MAX_DISTANCE) break;
     }
 
-  vec3 color = vec3((-travel_distance / 60.0) + 1.0);
+    return vec4(position, travel_distance);
+}
+
+void main() {
+  vec2 uv = vPos * 2.0 - 1.0;
+
+  // Move the camera
+  vec2 mouse_angle = vec2(-uMouse.x * 2.0, -uMouse.y * 2.0);
+
+  // Initialization
+  vec3 ray_origin = vec3(0, 0, -3);
+  // Vector length is normalized to be of length 1
+  vec3 ray_direction = normalize(vec3(uv, 1));
+  ray_origin.xz *= rot2d(mouse_angle.x);
+  ray_direction.xz *= rot2d(mouse_angle.x);
+  ray_origin.yz *= rot2d(mouse_angle.y);
+  ray_direction.yz *= rot2d(mouse_angle.y);
+
+  vec4 ray = ray_march(ray_origin, ray_direction);
+
+  float dif = get_light(ray.xyz);
+
+  vec3 color = vec3(dif);
   gl_FragColor = vec4(color, 1.0);
 }
